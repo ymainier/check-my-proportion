6051a708702cd8fd1fd8732cd503155d7682c0d7
Yann Mainier <yann.mainier@gmail.com>
2020-07-22 22:30:31 +0100

    First version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [stream, setStream] = useState(null);
+
+  const startCamera = () => {
+    if (stream) return;
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then((stream) => setStream(stream));
+  };
+  const stopCamera = () => {
+    if (!stream) return;
+    stream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+    setStream(null);
+  };
+
+  const style = { backgroundImage: `url(${imageSrc})` };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={style}>
+      <video
+        className="video"
+        autoPlay
+        ref={(video) => {
+          if (video) {
+            video.srcObject = stream;
+          }
+        }}
+      />
+      <input
+        className="file"
+        type="file"
+        onChange={(event) => {
+          const file = event.target.files[0];
+          setImageSrc(URL.createObjectURL(file));
+        }}
+      />
+      {stream ? (
+        <button className="camera" onClick={stopCamera}>Stop camera</button>
+      ) : (
+        <button className="camera" onClick={startCamera}>Start camera</button>
+      )}
     </div>
   );
 }
