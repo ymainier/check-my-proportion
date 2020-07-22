6bc852b71c3fcd3142dd38c2f84a753b2ee31ae7
Yann Mainier <yann.mainier@gmail.com>
2020-07-22 22:43:42 +0100

    Updates

---
+++ precache-manifest.167eaeb0758e3d7f84fef05d2c8b9810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5b0accc99e71632e5e0be61a984977",
+    "url": "/check-my-proportion/index.html"
+  },
+  {
+    "revision": "01592ead6375162487da",
+    "url": "/check-my-proportion/static/css/main.335e9810.chunk.css"
+  },
+  {
+    "revision": "5982f555172b0ab074fd",
+    "url": "/check-my-proportion/static/js/2.3befe0dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/check-my-proportion/static/js/2.3befe0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01592ead6375162487da",
+    "url": "/check-my-proportion/static/js/main.3010b581.chunk.js"
+  },
+  {
+    "revision": "de8758127d96baa832c3",
+    "url": "/check-my-proportion/static/js/runtime-main.19d708fd.js"
+  }
+]);
